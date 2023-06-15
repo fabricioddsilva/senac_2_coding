2d53734bdfc6630d03cb3dc94a05993c63d8999a
Fabrício Dias <fabriciodsilva3404@gmail.com>
2023-06-15 09:05:15 -0300

    Adicionando conexão com Banco Mysql

---
+++ Aula_Java/Evento-DA/src/test/java/senac/pe/Evento/DA/ApplicationTests.java
@@ -0,0 +1,13 @@
+package senac.pe.Evento.DA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
